03de6375cb8e1a38fda428389ce2ea00c77f662b
MrKolev <100051403+MrKolev@users.noreply.github.com>
2023-07-06 15:52:17 +0300

    Output a list of dummy events

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,3 +1,27 @@
+import { Link } from "react-router-dom"
+
+const DUMMY_EVENTS = [
+    {
+        id: "e1",
+        title: "some event"
+    },
+    {
+        id: "e2",
+        title: "Another event"
+    },
+    {
+        id: "e3",
+        title: "the big event"
+    },
+]
+
 export const EventsPage = () => {
-    return <h1>EventsPage</h1>
+    return <>
+        <h1>EventsPage</h1>
+        <ul>
+            {DUMMY_EVENTS.map(event => <li key={event.id}>
+                <Link to={event.id}>{event.title}</Link>
+            </li>)}
+        </ul>
+    </>
 }
