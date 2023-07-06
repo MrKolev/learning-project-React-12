916f68e1cb49dcc8cc781e3cacb2416445adc278
MrKolev <100051403+MrKolev@users.noreply.github.com>
2023-07-06 15:43:24 +0300

    Add properly working links to the MainNavigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,11 +32,11 @@ const router = createBrowserRouter([
   {path: '/', 
   element: <RootLayout/>,
 children: [
-  { path: '/', element: <HomePage /> },
-  { path: '/events', element: <EventsPage /> },
-  { path: '/events/:id', element: <EventDetailPage /> },
-  { path: '/events/new', element: <NewEventPage /> },
-  { path: '/events/:id/edit', element: <EditEventPage /> },
+  { index: true, element: <HomePage /> },
+  { path: 'events', element: <EventsPage /> },
+  { path: 'events/:id', element: <EventDetailPage /> },
+  { path: 'events/new', element: <NewEventPage /> },
+  { path: 'events/:id/edit', element: <EditEventPage /> },
 ]}
   
 
